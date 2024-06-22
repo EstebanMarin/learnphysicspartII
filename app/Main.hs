4167252758f30190f953e8cef2ed79978f131e7c
EstebanMarin <estebanmarin002@gmail.com>
2024-06-21 21:48:41 -0500

    compiling

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
-import Lib
+import Graphics.Gnuplot.Simple (Attribute (..), plotPath)
+import Lib (iHat, kHat, projectilePos, xComp, zComp, (*^), (^+^))
 
 main :: IO ()
-main = someFunc
+main =
+  let posInitial = 10 *^ kHat
+      velInitial = 20 *^ cos (pi / 6) *^ iHat ^+^ 20 *^ sin (pi / 6) *^ kHat
+      posFunc = projectilePos posInitial velInitial
+      pairs = [(xComp r, zComp r) | t <- [0, 0.01 ..], let r = posFunc t]
+      plotingPairs = takeWhile (\(_, z) -> z >= 0) pairs
+   in plotPath
+        [Title "Projectile Motion", XLabel "Horizontal Motion (m)", YLabel "Vertical Motion (m)", PNG "trajectory.png", Key Nothing]
+        plotingPairs
